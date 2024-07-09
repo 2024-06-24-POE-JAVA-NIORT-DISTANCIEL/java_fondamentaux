6110f9fdb07e51156a8d0a411e569270cf99389a
Christian Lisangola <jochri3@gmail.com>
2024-07-09 15:23:30 +0200

    Correction Tps

---
+++ src/journee_6_08_07_2024/tps/bibliotheque/Auteur.java
@@ -0,0 +1,19 @@
+package journee_6_08_07_2024.tps.bibliotheque;
+
+public class Auteur {
+    private String nom;
+    private boolean prime;
+
+    public Auteur(String nom, boolean prime) {
+        this.nom = nom;
+        this.prime = prime;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public boolean getPrix() {
+        return prime;
+    }
+}
---
+++ src/journee_6_08_07_2024/tps/construction/Habitable.java
@@ -0,0 +1,5 @@
+package journee_6_08_07_2024.tps.construction;
+
+public interface Habitable {
+    void occuper(int nombreDePersonnes);
+}
